20f443a58d0f6700c1124aaa1fee060691b62327
AngelaFerDel <angela.ferdel.1@educa.jcyl.es>
2026-01-12 12:24:22 +0100

    pr0608

--- ut06/pr0608/pr0608.ps1
+++ ut06/pr0608/pr0608.ps1
@@ -4,3 +4,34 @@ $kbsRequeridos = @("KB500123", "KB409999", "KB890830", "KB500556", "KB500321", "
 # Lista de parches INSTALADOS actualmente en el servidor 
 # Nota: Están desordenados y podría haber parches viejos que no son críticos.
 $kbsInstalados = @("KB100000", "KB500556", "KB409999", "KB100001", "KB890830", "KB200022")
+
+$kbsFaltantes = @()
+$kbsExtra = @()
+
+foreach ($datoRequerido in $kbsRequeridos) {
+    if ($datoRequerido -notin $kbsInstalados) {
+        $kbsFaltantes += $datoRequerido
+    }
+}
+
+foreach ($datoInstalado in $kbsInstalados) {
+    if ($datoInstalado -notin $kbsRequeridos) {
+        $kbsExtra += $datoInstalado
+    }
+}
+
+$porcentaje = (100 * ($kbsRequeridos.Length - $kbsFaltantes.Length) / $kbsRequeridos.Length).ToString() + "%"
+
+"=== AUDITORIA DE SEGURIDAD ==="
+"Total Requeridos: $($kbsRequeridos.Length)"
+"Total Instalados: $($kbsInstalados.Length)"
+""
+"ESTADO DE CUMPLIMIENTO: $porcentaje"
+""
+
+"[URGENTE] Parches Faltantes:"
+($kbsFaltantes -join ", ")
+
+""
+"[INFO] Parches 'Extra' instalados (No criticos):"
+($kbsExtra -join " - ")
